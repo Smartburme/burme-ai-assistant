c1ade22dd146533fbe51e649826c8f98e47ad55f
home <athuyrain@gmail.com>
2025-07-04 21:47:38 +0630

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,13 +1,11 @@
 // js/firebaseConfig.js
   const firebaseConfig = {
-   apiKey: "AIzaSyCK8xFznYs4FFuH-JBmXhX69I9iIdFC-DY",
-   authDomain: "smart-burme-ai.firebaseapp.com",
-   databaseURL: "https://smart-burme-ai-default-rtdb.firebaseio.com",
-   projectId: "smart-burme-ai",
-   storageBucket: "smart-burme-ai.firebasestorage.app",
-   messagingSenderId: "1057673784315",
-   appId: "1:1057673784315:web:b7004e00ce88b7ecd3b95e",
-   measurementId: "G-VDB178C50B"
+    apiKey: "AIzaSyAr7Hv2ApKtNTxF11MhT5cuWeg_Dgsh0TY",
+    authDomain: "smart-burme-app.firebaseapp.com",
+    projectId: "smart-burme-app",
+    storageBucket: "smart-burme-app.appspot.com",
+    messagingSenderId: "851502425686",
+    appId: "1:851502425686:web:f29e0e1dfa84794b4abdf7"
 };
 
 let firebaseApp;
